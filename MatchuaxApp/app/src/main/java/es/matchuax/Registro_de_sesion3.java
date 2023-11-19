9253664a342b5acf33e8a7d1abfd291f15585142
HugoH <hhervellagonzalez@gmail.com>
2023-11-19 19:54:00 +0100

    feat: Activity_registro2 realizada, activivity_registro3 realizada.

---
+++ MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion3.java
@@ -0,0 +1,12 @@
+package es.matchuax;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class Registro_de_sesion3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_de_sesion3);
+
+    }
+}
