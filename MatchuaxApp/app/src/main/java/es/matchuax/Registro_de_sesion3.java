ef28f75f2d020d46d07c4a1b0bd6fcb6e12d8fde
HugoH <hhervellagonzalez@gmail.com>
2023-11-20 13:59:56 +0100

    feat: Añadido el pais de origen a la activity de registro numero 3

--- MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion3.java
+++ MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion3.java
@@ -1,12 +1,41 @@
 package es.matchuax;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 public class Registro_de_sesion3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_de_sesion3);
+
+        String[] countries = getResources().getStringArray(R.array.countries_array);
+        String[] listaPaises = getResources().getStringArray(R.array.countries_array);
+
+// Crear un adaptador para el spinner
+        Spinner spinnerCountries = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listaPaises);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCountries.setAdapter(adapter);
+
+// Obtener el país seleccionado
+        spinnerCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                String selectedCountry = listaPaises[position];
+                // Aquí puedes trabajar con el país seleccionado
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Acción cuando no se selecciona nada
+            }
+        });
+
 
     }
 }
