16bd58f6725378434ec8d3d02079635b54b84a84
HugoH <hhervellagonzalez@gmail.com>
2023-11-20 14:30:02 +0100

    feat:Añadidio activity_registro_de_sesion4.xml.

---
+++ MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion4.java
@@ -0,0 +1,14 @@
+package es.matchuax;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Registro_de_sesion4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_de_sesion3);
+
+    }
+}
