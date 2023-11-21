e6225fd1c8022f5269965af1b19bf03d8afdfcdd
MARVAR <mariovarelavm@gmail.com>
2023-11-21 13:30:31 +0100

    Primer boceto match

--- MatchuaxApp/app/src/main/java/es/matchuax/Match.java
+++ MatchuaxApp/app/src/main/java/es/matchuax/Match.java
@@ -3,6 +3,10 @@ package es.matchuax;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class Match extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class Match extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        // Obtén la referencia del Spinner desde el diseño
+        Spinner spinner = findViewById(R.id.spinner);
+
+        // Datos para el Spinner
+        String[] languages = {"","Chat", "Mi Perfil", "Planes de Pago", "", "", "", "Ajustes"};
+
+        // Crea un adaptador y configúralo para el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, languages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
