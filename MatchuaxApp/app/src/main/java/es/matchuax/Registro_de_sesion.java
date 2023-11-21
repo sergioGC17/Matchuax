e2d34ecd09d449ad0b70f26523dbef66cff7feea
HugoH <125216404+Hhervellagonzalez@users.noreply.github.com>
2023-11-21 13:34:52 +0100

    Registro de sesion reralizdo(Sin funcionalidad).
    * feat: Añadidos varios edittext y spinenr funcional.
    
    * feat: 1er Activity realizada (Falta cambio en el fondo, 2 Activity)
    
    * feat: Activity_registro2 realizada, activivity_registro3 realizada.
    
    * feat: Añadido el pais de origen a la activity de registro numero 3
    
    * fix: Quitados los atras.png de activity_registro_de_sesion2.xml y activity_registro_de_sesion3.xml.
    
    * feat:Añadidio activity_registro_de_sesion4.xml.
    
    * feat:Realizacion del activity_registro_de_sesion4.xml.

--- MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion.java
+++ MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion.java
@@ -1,5 +1,11 @@
 package es.matchuax;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +16,29 @@ public class Registro_de_sesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_de_sesion);
+        // Definir la lista de universidades
+        /*String[] universidades = {"Seleciona tu Universidad", "Universidad Alfonso X el sabio","Universidad Francisco de vitoria","Universidad Europea"};
+
+        // Enlazar el Spinner con las opciones de universidades
+        Spinner universitySpinner = findViewById(R.id.Universidades);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, universidades);
+        universitySpinner.setAdapter(adapter);
+
+        // Agregar un escuchador al Spinner para manejar la selección
+        universitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String selectedUniversity = universidades[position];
+                // Realizar acciones con la universidad seleccionada
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Manejar caso en que no se selecciona nada (si es necesario)
+            }
+        });
+        */
     }
 }
