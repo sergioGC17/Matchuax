9d38b2e6c579dce10c4d8539ce53f943167f94a8
dmoreno09 <149317387+dmoreno09@users.noreply.github.com>
2023-11-28 00:18:59 +0100

    Desarrollo (#16)
    * Agrego el activity de chats
    
    * Agrego el activity de Conversaciones
    
    * Arreglo en Strings
    
    * Subo el activity de Ubicacion
    
    * feat: Cambio en colors.xml.
    
    * PR de Hugo  a Desarrollo (#20)
    
    * feat: Logo Añadido y cambio en el manifest.
    
    * feat: Cambios en el estilo
    
    * Registro de sesion reralizdo(Sin funcionalidad). 
    
    * feat: Añadidos varios edittext y spinenr funcional.
    
    * feat: 1er Activity realizada (Falta cambio en el fondo, 2 Activity)
    
    * feat: Activity_registro2 realizada, activivity_registro3 realizada.
    
    * feat: Añadido el pais de origen a la activity de registro numero 3
    
    * fix: Quitados los atras.png de activity_registro_de_sesion2.xml y activity_registro_de_sesion3.xml.
    
    * feat:Añadidio activity_registro_de_sesion4.xml.
    
    * feat:Realizacion del activity_registro_de_sesion4.xml.
    
    * Ajustes del perfil (Sin funcionalidad). (#15)
    
    * feat: creacion del activity_ajustes_de_perfil.xml y fotos añadidas.
    
    * feat: realizacion de la activity_ajustes_de_perfil.xml
    
    * feat: cambios de la activity_ajustes_de_perfil.xml
    
    * feat: Cambio en el nombre de algunas imagenes.
    
    * feat: Cambio en ids.
    
    * feat: botones funcionales
    
    ---------
    
    Co-authored-by: sergioGC17 <125812029+sergioGC17@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sergio <sgomecuq@mmyuax.com>
    Co-authored-by: sergioGC17 <125812029+sergioGC17@users.noreply.github.com>
    Co-authored-by: HugoH <hhervellagonzalez@gmail.com>
    Co-authored-by: HugoH <125216404+Hhervellagonzalez@users.noreply.github.com>

--- MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion2.java
+++ MatchuaxApp/app/src/main/java/es/matchuax/Registro_de_sesion2.java
@@ -1,24 +1,23 @@
 package es.matchuax;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Button;
 
-public class Chats extends AppCompatActivity {
+public class Registro_de_sesion2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chats);
+        setContentView(R.layout.activity_registro_de_sesion2);
 
-        LinearLayout linearLayout = findViewById(R.id.LinearLayoutMariaFondo);
-        linearLayout.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.siguiente);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Chats.this, Conversaciones.class);
+                Intent intent = new Intent(Registro_de_sesion2.this, Registro_de_sesion3.class);
                 startActivity(intent);
             }
         });
