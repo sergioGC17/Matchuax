4be0e4b4451ed57c2ca37d154c14334824b7741e
MARVAR <134282944+MARVAR-04@users.noreply.github.com>
2023-11-28 09:36:10 +0100

    Mario (#25)
    * Agrego Informacion_adicional.txt, Listado_interfaces.txt, Logo.png y Mockup.png
    
    * Actualizo el logo.png
    
    * borro el logo
    
    * Agrego el logo
    
    * Delete Logo.png
    
    * Agrego el logo
    
    logo agregado
    
    * Delete Logo.png
    
    * Agrego el logo.png
    
    Logo.png agregado
    
    * errores
    
    * error
    
    * actualizacion informacion_adicional
    
    actualizo el txt
    
    * Actualizo informacion_adicional
    
    actualizo el txt
    
    * Delete Listado_interfaces.txt (Ahora se encuentra en Notion)
    
    * Delete Informacion_adicional.txt (Esta en el notion)
    
    * Boceto de diseño Mega Match (#19)
    
    * Megamatch (#21)
    
    * Boceto de diseño Mega Match
    
    * He añadido el boto de marcha atras y una imagen para igualar el modelo al mockup
    
    * Perfil de las personas (#24)
    
    * commit
    
    * Primer boceto Perfil de la persona
    
    * Se ha eliminado el boton editar que no era necesario, se ha añadido la funcion back en el java y se han cambiado los id del xml para facilitar su uso
    
    ---------
    
    Co-authored-by: Sergio <sgomecuq@mmyuax.com>
    
    ---------
    
    Co-authored-by: Sergio <sgomecuq@mmyuax.com>
    Co-authored-by: sergioGC17 <125812029+sergioGC17@users.noreply.github.com>
    Co-authored-by: HugoH <125216404+Hhervellagonzalez@users.noreply.github.com>

--- MatchuaxApp/app/src/main/java/es/matchuax/Megamatch.java
+++ MatchuaxApp/app/src/main/java/es/matchuax/Megamatch.java
@@ -1,8 +1,10 @@
 package es.matchuax;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Megamatch extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Megamatch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_megamatch);
+
+        ImageView imageView = findViewById(R.id.atras);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Megamatch.this, Chats.class);
+                startActivity(intent);
+            }
+        });
     }
 }
