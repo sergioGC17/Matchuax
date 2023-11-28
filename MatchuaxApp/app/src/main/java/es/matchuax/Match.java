f871a2508c2531359a57ff762ecd2a2cac35d6c2
MARVAR <134282944+MARVAR-04@users.noreply.github.com>
2023-11-28 12:56:03 +0100

    Update Match.java

--- MatchuaxApp/app/src/main/java/es/matchuax/Match.java
+++ MatchuaxApp/app/src/main/java/es/matchuax/Match.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import es.varela.mario.machtuax.R;
+
 public class Match extends AppCompatActivity {
 
     @Override
@@ -18,6 +20,17 @@ public class Match extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Match.this, Inicio.class);
+                startActivity(intent);
+            }
+        });
+
+        // Configuración del OnClickListener para el ImageView "matchchat"
+        ImageView imageViewMatchChat = findViewById(R.id.matchchat);
+        imageViewMatchChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción al hacer clic en el ImageView "matchchat"
+                Intent intent = new Intent(Match.this, Chats.class);
                 startActivity(intent);
             }
         });
