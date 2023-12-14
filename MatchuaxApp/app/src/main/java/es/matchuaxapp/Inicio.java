03f7077eadd43d36cda530e28eac52f721894fe2
MARVAR <134282944+MARVAR-04@users.noreply.github.com>
2023-12-14 23:44:58 +0100

    Update Inicio.java
    Se han agregado cambios

--- MatchuaxApp/app/src/main/java/es/matchuaxapp/Inicio.java
+++ MatchuaxApp/app/src/main/java/es/matchuaxapp/Inicio.java
@@ -1,22 +1,36 @@
 package es.matchuaxapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Inicio extends AppCompatActivity {
+
+    private static final int[] IMAGENES_PERSONAS = {
+            R.drawable.maria,
+            R.drawable.paula,
+            R.drawable.andrea,
+            R.drawable.lucia,
+            R.drawable.martina,
+            R.drawable.sofia,
+            R.drawable.teresa,
+            // Agrega más recursos de imágenes según sea necesario
+    };
+
+    private ImageView imageViewPersona;
+    private TextView textViewNombre;
+    private TextView textViewEdificio;
+    private int indiceActual;  // Asegúrate de actualizar este índice según tu lógica
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-
 
         ImageView imageView = findViewById(R.id.next);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -27,62 +41,101 @@ public class Inicio extends AppCompatActivity {
             }
         });
 
-        ImageView imageView2 = findViewById(R.id.megamatch);
-        imageView2.setOnClickListener(new View.OnClickListener() {
+
+
+        ImageView imageView4 = findViewById(R.id.ajustes);
+        imageView4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Inicio.this, Megamatch.class);
+                Intent intent = new Intent(Inicio.this, Ajustes_del_perfil.class);
                 startActivity(intent);
             }
         });
 
-        ImageView imageView3 = findViewById(R.id.match);
-        imageView3.setOnClickListener(new View.OnClickListener() {
+        ImageView imageView5 = findViewById(R.id.user);
+        imageView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Inicio.this, Ajustes_del_perfil.class); // Cambia PerfilDeUsuario por el nombre correcto de tu clase de perfil de usuario
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView6 = findViewById(R.id.chat);
+        imageView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Inicio.this, Chats.class); // Cambia ListaDeChats por el nombre correcto de tu clase de lista de chats
+                startActivity(intent);
+            }
+        });
+
+        // Obtén referencias a las vistas
+        imageViewPersona = findViewById(R.id.persona);
+        textViewNombre = findViewById(R.id.editText);
+        textViewEdificio = findViewById(R.id.textView);
+
+        // Configura una imagen aleatoria y muestra la información
+        indiceActual = obtenerIndiceAleatorio();
+        mostrarInformacionPersona();
+
+        // Manejador de clic para el botón "Match"
+        ImageView imageViewMatch = findViewById(R.id.match);
+        imageViewMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtén la información actual de la persona en la actividad Inicio
+                String nombrePersona = textViewNombre.getText().toString();
+                int idImagenPersona = IMAGENES_PERSONAS[indiceActual];
+                String edificio = textViewEdificio.getText().toString();
+
+                // Crea un Intent y pasa la información a la actividad Match
                 Intent intent = new Intent(Inicio.this, Match.class);
+                intent.putExtra("NombrePersona", nombrePersona);
+                intent.putExtra("IdImagenPersona", idImagenPersona);
+                intent.putExtra("Edificio", edificio);
                 startActivity(intent);
             }
         });
 
-
-
-        Spinner spinner = findViewById(R.id.spinner);
-        // Datos para el Spinner
-        String[] opciones = {"", "Chat", "Mi Perfil", "Planes de Pago", "", "", "", "Ajustes"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        // Manejador de clic para el botón "Megamatch"
+        ImageView imageViewMegamatch = findViewById(R.id.megamatch);
+        imageViewMegamatch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                String spinner = adapter.getItem(position);
-                Intent intent;
-                switch (spinner) {
-                    case "Chat":
-                        intent = new Intent(Inicio.this, Chats.class);
-                        startActivity(intent);
-                        break;
-                    case "Mi Perfil":
-                        intent = new Intent(Inicio.this, Perfil.class);
-                        startActivity(intent);
-                        break;
-                    case "Planes de Pago":
-                        intent = new Intent(Inicio.this, Inicio.class);
-                        startActivity(intent);
-                        break;
-                    case "Ajustes":
-                        intent = new Intent(Inicio.this, Ajustes_del_perfil.class);
-                        startActivity(intent);
-                        break;
-                }
-            }
+            public void onClick(View v) {
+                // Obtén la información actual de la persona en la actividad Inicio
+                String nombrePersona = textViewNombre.getText().toString();
+                int idImagenPersona = IMAGENES_PERSONAS[indiceActual];
+                String edificio = textViewEdificio.getText().toString();
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+                // Crea un Intent y pasa la información a la actividad Megamatch
+                Intent intent = new Intent(Inicio.this, Megamatch.class);
+                intent.putExtra("NombrePersona", nombrePersona);
+                intent.putExtra("IdImagenPersona", idImagenPersona);
+                intent.putExtra("Edificio", edificio);
+                startActivity(intent);
             }
         });
+
+        // Otros manejadores de clic para imágenes...
+    }
+
+    private int obtenerIndiceAleatorio() {
+        Random random = new Random();
+        return random.nextInt(IMAGENES_PERSONAS.length);
+    }
+
+    private void mostrarInformacionPersona() {
+        int idImagen = IMAGENES_PERSONAS[indiceActual];
+        imageViewPersona.setImageResource(idImagen);
+
+        String[] nombres = {"Maria 20 años", "Paula 18 años", "Andrea 21 años", "Lucia 22 años", "Martina 19 años", "Sofia 24 años", "Teresa 23 años"};
+        String[] edificios = {"Edificio A", "Edificio B", "Edificio C", "Edificio D"};
+
+        String nombre = nombres[new Random().nextInt(nombres.length)];
+        String edificio = edificios[new Random().nextInt(edificios.length)];
+
+        textViewNombre.setText(nombre);
+        textViewEdificio.setText(edificio);
     }
 }
